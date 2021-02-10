ff19a9d97b3c7ca1771cc4968b51153c8bbdb927
PeterAlexandre <petermiguel99@gmail.com>
2021-02-10 17:47:12 -0300

    update user_field into a writable related_field

--- auction/users/serializers.py
+++ auction/users/serializers.py
@@ -1,10 +1,22 @@
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from .models import User, Address
 
 
-class AddressSerializer(serializers.HyperlinkedModelSerializer):
+class AddressSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field="username",
+        queryset=User.objects.all(),
+        validators=[
+            UniqueValidator(
+                queryset=Address.objects.all(),
+                message="An address already exists for this user."
+            )
+        ]
+    )
+
     class Meta:
         model = Address
         fields = [
@@ -18,11 +30,6 @@ class AddressSerializer(serializers.HyperlinkedModelSerializer):
             "country",
             "user",
         ]
-
-
-class PublicAddressField(serializers.RelatedField):
-    def to_representation(self, value):
-        return (f"{value.city} / {value.state} - {value.country}")
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -41,6 +48,7 @@ class UserSerializer(serializers.ModelSerializer):
             "phone_number",
             "cpf",
             "birth_date",
+            "date_joined",
             "address",
             "is_staff",
             "is_active",
@@ -49,12 +57,22 @@ class UserSerializer(serializers.ModelSerializer):
             "password": {"write_only": True, "min_length": 8, "required": False},
             "email": {"required": True},
         }
-        read_only_fields = (
+        read_only_fields = [
             "id",
             "is_staff",
             "is_active",
-            "address"
-        )
+            "address",
+            "date_joined",
+        ]
+
+    def get_fields(self):
+        fields = super().get_fields()
+
+        request = self.context.get("request")
+        if request.method == "POST":
+            fields["password"].required = True
+
+        return fields
 
     def create(self, validated_data):
         validated_data["password"] = make_password(validated_data.get("password"))
@@ -63,6 +81,32 @@ class UserSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         validated_data.pop("password", None)
         return super().update(instance, validated_data)
+
+
+class PublicAddressField(serializers.RelatedField):
+    def to_representation(self, value):
+        return (f"{value.city} / {value.state} - {value.country}")
+
+
+class PublicUserSerializer(serializers.ModelSerializer):
+    address = PublicAddressField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "url",
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "address",
+            "is_active",
+        ]
+        read_only_fields = [
+            "id",
+            "is_active",
+            "address",
+        ]
 
 
 class ChangePasswordSerializer(serializers.ModelSerializer):
